531a1fc8e53bf3d9a31a71381bc3fc67f8b26f30
mdemo <mengdesen09@qq.com>
2013-11-12 17:45:37 +0800

    add to npm

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ shui
 ====
 
 a whois npm module 
+
+
+##install
+
+```
+npm install shui -g
+```
+
+##CLI 
+
+
+```
+shui "baidu.com"
+```
+### Node.js
+
+```
+var shui = require('./shui');
+shui('baidu.com',function(result){
+	console.log(result);
+});
+
+```
--- shui.js
+++ shui.js
@@ -1,12 +1,25 @@
 var request = require('request');
 
 module.exports = function(domain,callback){
+	if(domain.cli){
+		domain = domain._[0];
+	}
 	request('https://domai.nr/api/json/info?q='+domain, function (error, response,body) {
  		if (!error && response.statusCode == 200) {
-    	 	callback(JSON.parse(body));
+ 			if(typeof callback ==='function'){
+ 				callback(JSON.parse(body));
+ 			}
+ 			else{
+ 				console.log(JSON.parse(body));
+ 			}
  		}
  		else{
- 			callback(error);
+ 			if(typeof callback ==='function'){
+ 				callback(error);
+ 			}
+ 			else{
+ 				console.log(error);
+ 			}
  		}
 	});
 };
